bb056568b85657bbc473d8e93b76c5c49ec2c5f7
Sharjeelkhhan <133125354+Sharjeelkhhan@users.noreply.github.com>
2023-07-20 16:49:20 +0500

    Delete data.php

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Define database connection constants
-define("DB_HOST","localhost"); // Hostname of the database server
-define("DB_USER","root"); // Username for database access
-define("DB_PASSWORD",""); // Password for database access
-define("DB_NAME","form_submission"); //database name
-// Create a new MySQLi object for database connection
-$mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
-?>
